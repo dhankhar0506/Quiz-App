f3a154dd70e4fc9527cd9e758ca2be69a039c6c7
dhankhar0506 <dhankhargaurav51@gmail.com>
2023-05-18 08:52:17 +0530

    Quiz App

---
+++ src/Data/QuizData.js
@@ -0,0 +1,63 @@
+export const QuizData = [
+    {
+        question: "What does HTML stand for?",
+        options: ["Hyper Tag Markup Language", "Hyper Text Markup Language", "Hyperlinks Text Mark Language", "Hyperlinking Text Marking Language"],
+        answer:2
+    },
+    {
+        question: "What symbol indicates a tag?",
+        options: ["Angle brackets e.g.", "Curved brackets e.g. {,}", "Commas e.g. ','", "Exclamation marks e.g. !"],
+        answer:1
+    },
+    {
+        question: "The item present within the angled brackets in an HTML tag is",
+        options:[ "dentifier", "data", "tags", "text"],
+        answer: 1
+    },
+    
+    {
+        question: "Which of these is a genuine tag keyword?",
+        options:[ "Header", "Bold", "Body", "Image"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    
+    {
+        question: "Which of the following JavaScript cannot do?",
+        options: ["JavaScript can react to events", "JavaScript can manipulate HTML element", "JavaScript can be use to validate data", " All of the Above"],
+        answer: 4
+    },
+
+  
+
+    {
+        question: "The NOSHADE attribute ?",
+        options: ["displays the example in red", "displays the line in dark gray", "displays the line in red", "defines the thickness of the line"],
+        answer: 3
+    },
+    
+    {
+        question: "Which language runs in a web browser?",
+        options:[ "Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    
+    
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+    {
+        question: "What symbol indicates a tag?",
+        options: ["Angle brackets e.g.", "Curved brackets e.g. {,}", "Commas e.g. ','", "Exclamation marks e.g. !"],
+        answer:2
+    }        
+   
+   
+
+];
